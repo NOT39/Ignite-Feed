b2a00d9a3c2de1a86df8f59d7e995caa2153ba59
Not <110069021+NOT39@users.noreply.github.com>
2023-02-20 13:45:42 -0300

    Feat: Create post component

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,3 +1,5 @@
+import { PencilLine } from 'phosphor-react'
+
 import styles from './Sidebar.module.css'
 
 export const Sidebar = () => {
@@ -17,6 +19,7 @@ export const Sidebar = () => {
 
       <footer>
         <a href="#">
+          <PencilLine size={20}/>
           Editar seu perfil
         </a>
       </footer>
